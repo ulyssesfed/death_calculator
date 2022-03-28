c87bad1c7531dd072e73fb932f29a6e037acc491
gf211097 <ulyfleen@gmail.com>
2022-03-28 15:55:52 +0100

    im pretty sure it works now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Date;
 
@@ -8,6 +10,36 @@ public class Main {
 
 
     public static void main(String[] args) {
+        String[] deathM = {"very boring", "boring", "mundane", "cringe", "alright", "epic", "amazing", "litterally the coolest death of all time"};
+        String[] deathHVB = {"you die in your sleep", "you got cancer", "im god and even i cant remember thats how boring your death was"};
+        String[] deathHB = {"you died in a car crash", "you fell of a ladder", "you die of a heart attack"};
+        String[] deathHM = {"you had a stroke", " you died of respiratory infections", "you died of diabetes"};
+        String[] deathHC = {"you died like kenneth pinyon", "lmao you died when you bled out after getting a paper clip"};
+        String[] deathHA = {"you die laughing at something truly funny", "you die among friends", "you got murdered"};
+        String[] deathHE = {"you had a tortoise dropped on your head by an eagle", "you die like micheal warner", "yoe die like king adolf frederick of sweden"};
+        String[] deathHAm = {"you die saving the life of another", "i dont know how but you jumped into a black hole", "you suffocated in titties"};
+        String deathHL = "you invented time travel and traveled back to kill yourself, there is not cooler death than a paradoxical death";
+        String deathhow = "";
+        Random rand = new Random();
+        int deathR = rand.nextInt(7);
+        if (deathR == Arrays.asList(deathM).indexOf("very boring")) {
+            deathhow = deathHVB[rand.nextInt(2)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("boring")) {
+            deathhow = deathHB[rand.nextInt(2)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("mundane")) {
+            deathhow = deathHM[rand.nextInt(2)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("cringe")) {
+            deathhow = deathHC[rand.nextInt(1)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("alright")) {
+            deathhow = deathHA[rand.nextInt(2)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("epic")) {
+            deathhow = deathHE[rand.nextInt(2)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("amazing")) {
+            deathhow = deathHAm[rand.nextInt(2)];
+        } else if (deathR == Arrays.asList(deathM).indexOf("litterally the coolest death of all time")) {
+            deathhow = deathHL;
+        }
+
         //noinspection deprecation
         int year = d.getYear() + 1900;
         //noinspection deprecation
@@ -43,17 +75,36 @@ public class Main {
                 pi++;
             }
         }
-        int days = (30 * monthRaw) - (modifier) + day; //this causes issues for some reason
-        int pdays = ((30 * pmonthRaw) - (pmodifier)) + pday;
+        int days = (30 * monthRaw) - modifier + day;
+        int pdays = ((30 * pmonthRaw) + (pmodifier)) + pday + 1;// the plus 1 fixes things but i dont know why or how and now im scared
         int yid = (int) (((year * 365.2422) + days) - 365.2422); //does now account for leap years
         int pYid = (int) (((pyear * 365.2422) + pdays) - 365.2422);
-        System.out.println(yid);
-        System.out.println(pYid);
-        System.out.println(yid - pYid);
-
+        int dol = yid - pYid;
+        int hours = dol * 24;
+        int minutes = hours * 60;
+        int seconds = minutes * 60;
+        String a = "there have been ";
+        System.out.println(a + yid + " days since 1 A.D.");
+        System.out.println(a + pYid + " days between 1 A.D. and your birth");
+        System.out.println(a + dol + " days in your life");
+        System.out.println(a + hours + " hours in your life");
+        System.out.println(a + minutes + " minutes in your life");
+        System.out.println(a + seconds + " seconds in your life");
         int TA = year - pyear;
-        System.out.println(TA);
+        int TD;
+        if (pday > days) {
 
+            System.out.println("you are " + TA);
+            TD = rand.nextInt(110 - 73) + 73 - TA;
+            System.out.println(TD);
+        } else {
+            System.out.println("you are " + (TA - 1));
+            TA = TA - 1;
+            TD = rand.nextInt(110 - 73) + 73 - TA - 1;
+            System.out.println(TD);
+        }
+
+        System.out.println("you will die in " + TD + " years, you will be " + (TD + TA) + " and your death will be: " + deathM[deathR] + ", in fact " + deathhow);
     }
 
     private static int getInput(String prompt) {
