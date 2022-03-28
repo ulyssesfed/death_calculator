d32c9ddaeb3a411b1e39dcef0bedfaddbcf98f40
gf211097 <ulyfleen@gmail.com>
2022-03-28 13:21:10 +0100

    still doesnt work properly but i found whats causing the issues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 import java.util.Date;
 
 public class Main {
-    private static Date d = new Date();
+    private static final Date d = new Date();
 
 
     public static void main(String[] args) {
+        //noinspection deprecation
         int year = d.getYear() + 1900;
+        //noinspection deprecation
         int day = d.getDate();
+        //noinspection deprecation
         int monthRaw = d.getMonth();
         int modifier = 0;
         int pyear = getInput("year of birth");
@@ -19,11 +22,10 @@ public class Main {
         int i = 1;
         int pi = 1;
         while (i < monthRaw) {
-            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10) {
                 modifier = modifier + 1;
                 i++;
-            } else if (i == 4 || i == 6 || i == 9 || i == 11) {
-                modifier = modifier + 0;
+            } else if (i == 4 || i == 6 || i == 9) {
                 i++;
             } else if (i == 2) {
                 modifier = modifier - 2;
@@ -35,14 +37,13 @@ public class Main {
                 pmodifier = pmodifier + 1;
                 pi++;
             } else if (pi == 4 || pi == 6 || pi == 9 || pi == 11) {
-                pmodifier = pmodifier + 0;
                 pi++;
             } else if (pi == 2) {
                 pmodifier = pmodifier - 2;
                 pi++;
             }
         }
-        int days = (30 * monthRaw) - (modifier) + day;
+        int days = (30 * monthRaw) - (modifier) + day; //this causes issues for some reason
         int pdays = ((30 * pmonthRaw) - (pmodifier)) + pday;
         int yid = (int) (((year * 365.2422) + days) - 365.2422); //does now account for leap years
         int pYid = (int) (((pyear * 365.2422) + pdays) - 365.2422);
@@ -50,8 +51,7 @@ public class Main {
         System.out.println(pYid);
         System.out.println(yid - pYid);
 
-        int Yob = getInput("what year were you born in");
-        int TA = year - Yob;
+        int TA = year - pyear;
         System.out.println(TA);
 
     }
